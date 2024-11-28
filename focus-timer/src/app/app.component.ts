0a4581a06f96c017e452d20ccab2141f311e5897
Miguel Verdelho <miguel@verdelho.net>
2024-11-28 08:26:17 +0000

    first angular commit

---
+++ focus-timer/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { TimerComponent } from "./timer/timer.component";
+import { HeaderComponent } from "./header/header.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [TimerComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'focus-timer';
+}
